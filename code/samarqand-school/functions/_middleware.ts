45def5f929e2d06ce1733faff9c86a5e362602ad
Sjunja <111452623+Sjunja@users.noreply.github.com>
2026-01-26 14:57:06 +0500

    Add middleware SPA fallback

---
+++ code/samarqand-school/functions/_middleware.ts
@@ -0,0 +1,23 @@
+export const onRequest: PagesFunction = async ({ request, env, next }) => {
+  const url = new URL(request.url);
+
+  if (request.method !== 'GET' && request.method !== 'HEAD') {
+    return next();
+  }
+
+  const pathname = url.pathname;
+  if (pathname.startsWith('/api/')) {
+    return next();
+  }
+
+  if (pathname.startsWith('/assets/') || pathname.startsWith('/images/')) {
+    return next();
+  }
+
+  if (pathname.includes('.')) {
+    return next();
+  }
+
+  const indexUrl = new URL('/index.html', url);
+  return env.ASSETS.fetch(new Request(indexUrl, request));
+};
